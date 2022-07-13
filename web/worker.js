fe41bfb30db33732409461e197ad3857a75663de
Maximilian Heisinger <mail@maxheisinger.at>
2022-07-14 00:26:50 +0200

    Make web-based BMP to fdf-map work

--- web/worker.js
+++ web/worker.js
@@ -3,6 +3,8 @@ importScripts('./dfmapcompressorpp.js');
 let progresscb = (p) => postMessage({'type': 'progress', 'progress': p});
 
 Module().then(function(m) {
+    const FS = m.FS;
+    const WORKERFS = m.WORKERFS;
     onmessage = msg => {
 	msg = msg.data;
 	let l = new m.StringList;
@@ -11,11 +13,12 @@ Module().then(function(m) {
 	    l.push_back('/input/' + f.name);
 	}
 
-	FS.mkdir('/work');
+	FS.mkdir('/input');
 	FS.mount(WORKERFS, { files: msg['files'] }, '/input');
 
 	let res = m.process_images(msg["tileWidth"], msg["tileHeight"], l, progresscb);
-	postMessage({'type': "result", 'result': res});
+	let resFile = FS.readFile(res);
+	postMessage({'type': "result", 'name': res, 'data': resFile});
     };
 
     postMessage({'type': "ready"});
